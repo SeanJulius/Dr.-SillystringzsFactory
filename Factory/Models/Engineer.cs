91a0db2bc0b63ebc9931eec04f5a4b2896490268
Sean <crimsonsand@live.com>
2022-08-14 11:52:57 -0700

    Add initial code to controllers, models, and views

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,2 +1,18 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+    public string Name { get; set; }
+    public int EngineerId { get; set; }
+    public string DateHired { get; set; }
+
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }    
+  }
+}
